34a95cba74485b737087d22142ad70bdc0ade235
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-03-13 20:56:23 +0000

    more heroku tries

--- flaskrun.py
+++ flaskrun.py
@@ -38,6 +38,6 @@ def hello_world():
 
 if __name__ == '__main__':
     if 'PORT' in os.environ:
-        app.run(port=int(os.environ.get('PORT')))
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
     else:
         app.run()
