2a196237b63da3b4f31cbf0abe889c57217f6bf2
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-03-31 05:09:12 +0100

    Added failing integration test

---
+++ tests/integration.py
@@ -0,0 +1,44 @@
+"""
+Message Maker
+
+Copyright (C) 2018  Pedro Rodrigues <prodrigues1990@gmail.com>
+
+This file is part of messagemaker.
+
+Message Maker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 2 of the License.
+
+Message Maker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Message Maker.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from ddt import ddt, data, unpack
+from metar import Metar
+
+from messagemaker.atis import *
+import settings
+
+@ddt
+class TestIntegration(unittest.TestCase):
+
+    def setUp(self):
+        self.airport = settings.AIRPORTS['LPPT']
+        self.transition = settings.TRANSITION
+        self.letter = 'A'
+        self.rwy = '03'
+    
+    @data(
+        ('METAR LPPT 191800Z 35015KT 9999 SCT027 11/06 Q1016', '03')
+    )
+    @unpack
+    def test_message_doesnotfail(self, metar, rwy):
+        message(metar, rwy, self.letter)
+
