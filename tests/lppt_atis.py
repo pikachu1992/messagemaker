f9646cf63253f6692271cc868d152694b2e1f34f
becas22 <benyreis@hotmail.com>
2018-03-31 16:15:42 +0100

    Tests for Cloud field

--- tests/lppt_atis.py
+++ tests/lppt_atis.py
@@ -112,8 +112,24 @@ T POSITION U FOR DEPARTURE, IF UNABLE ADVISE BEFORE TAXI]'),
     @data(
         ('METAR LPPT 191800Z 35015KT 11/06 Q1016',
         ''),
+        ('METAR LPPT 191800Z 35015KT FEW003 11/06 Q1016',
+        '[CLD] [FEW] {300} [FT]')
         ('METAR LPPT 191800Z 35015KT SCT003 11/06 Q1016',
         '[CLD] [SCT] {300} [FT]')
+        ('METAR LPPT 191800Z 35015KT BKN003 11/06 Q1016',
+        '[CLD] [BKN] {300} [FT]')
+        ('METAR LPPT 191800Z 35015KT OVC003 11/06 Q1016',
+        '[CLD] [OVC] {300} [FT]')
+        ('METAR LPPT 191800Z 35015KT FEW030 11/06 Q1016',
+        '[CLD] [FEW] {3000} [FT]')
+        ('METAR LPPT 191800Z 35015KT FEW100 11/06 Q1016',
+        '[CLD] [FEW] {10000} [FT]')
+        ('METAR LPPT 191800Z 35015KT FEW000 11/06 Q1016',
+        '[CLD] [FEW] {000} [FT]')
+        ('METAR LPPT 191800Z 35015KT FEW020CB 11/06 Q1016',
+        '[CLD] [FEW] [CB] {2000} [FT]')
+        ('METAR LPPT 191800Z 35015KT FEW040TCU 11/06 Q1016',
+        '[CLD] [FEW] [TCU] {4000} [FT]')
     )
     @unpack
     def test_sky(self, metar, expected):
